77190c4977cb79b6546077ca032345a12592b346
Carlos Mora <ozonosp@gmail.com>
2016-03-22 20:09:10 +0100

    Nuevas entidades

---
+++ src/DataImport.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+* 2006-2015 Lucid Networks
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+*
+* DISCLAIMER
+*
+*  Date: 9/2/16 18:57
+*  @author Networkkings <info@lucidnetworks.es>
+*  @copyright  2006-2015 Lucid Networks
+*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*/
+
+namespace Lucid\Anfix;
+
+class DataImport extends BaseModel
+{
+    protected $applicationId = '3';
+    protected $update = false;
+    protected $create = true;
+    protected $delete = false;
+
+}
